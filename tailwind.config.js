04f801e827c2a16ff28b71dde12b0b713b8de98a
michirem <dariuss@gmx.at>
2021-04-08 16:00:31 +0200

    completed about me section content

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,31 @@ module.exports = {
     },
     darkMode: 'class', // or 'media' or 'class'
     theme: {
-      extend: {},
+      screens: {
+        'xs': '480px',
+        // => @media (min-width: 480px) { ... }
+
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      } ,
+      extend: {
+        inset: {
+          '17': '68px',
+          '26': '104px'
+        }
+      },
     },
     variants: {
       extend: {},
